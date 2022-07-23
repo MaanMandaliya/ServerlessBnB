7abfa72bd4b5ed74f46d8e61a4a1626b6c7ff459
Abhishek karthik Manikandan <ab620101@dal.ca>
2022-07-23 12:59:35 -0300

    Added card for homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import OrderFeedback from "./Custom-Components/Food/orderFeedback";
 import Rooms from "./Custom-Components/Hotel/Rooms";
 import RoomFeedback from "./Custom-Components/Hotel/RoomFeedback";
 
+
 function App() {
   return (
     <div>
@@ -35,6 +36,7 @@ function App() {
           <Route path="/hotelFeedback" element={<RoomFeedback />} />
         </Routes>
       </Router>
+
     </div>
   );
 }
